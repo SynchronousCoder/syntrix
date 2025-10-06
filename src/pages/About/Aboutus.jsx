f51a3ca82d8412d1ad3afe6138e0ab393ffcce34
Aryan Tyagi <dontfollowmann@gmail.com>
2025-10-07 04:36:04 +0530

    fix: resolve Company component import issue

--- src/pages/About/Aboutus.jsx
+++ src/pages/About/Aboutus.jsx
@@ -1,20 +1,5 @@
-// import React from 'react'
-// import Company from '../../components/AboutMe/company';
-
-
-// const Aboutus = () => {
-//   return (
-//     <div className='h-screen w-full bg-red-100'>
-//       <Company />
-//     </div>
-//   )
-// }
-
-// export default Aboutus
-
-
 import React from 'react'
-import Company from '../../components/AboutMe/company'
+import Company from '../../components/AboutMe/Company'
 
 const Aboutus = () => {
   return (
