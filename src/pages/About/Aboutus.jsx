1a948317363648ffb669f2478000bf2d80ea8945
Aryan Tyagi <dontfollowmann@gmail.com>
2025-10-07 04:19:04 +0530

    first deployed

--- src/pages/About/Aboutus.jsx
+++ src/pages/About/Aboutus.jsx
@@ -1,3 +1,18 @@
+// import React from 'react'
+// import Company from '../../components/AboutMe/company';
+
+
+// const Aboutus = () => {
+//   return (
+//     <div className='h-screen w-full bg-red-100'>
+//       <Company />
+//     </div>
+//   )
+// }
+
+// export default Aboutus
+
+
 import React from 'react'
 import Company from '../../components/AboutMe/company'
 
